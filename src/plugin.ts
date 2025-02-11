58acc12a94f55d842a6c708bdfab698686e671ce
Junyeong Heo <hhjjyy1122@gmail.com>
2025-02-11 21:44:06 +0800

    ✨ Implemented timestamp for interval to the specific file feature

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,13 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
-import { AppendTimestampToFile } from "./actions/append-to-file";
+import { AppendTimestampToFile, AppendTimeintervalToFile } from "./actions/append-to-file";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
 
 // Register the actions.
 streamDeck.actions.registerAction(new AppendTimestampToFile());
+streamDeck.actions.registerAction(new AppendTimeintervalToFile());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
